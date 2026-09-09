c414b8b5b582c649ad99e180ca40c1a8be6f4424
Shashwat Ratna <Shaswatratna@gmail.com>
2026-09-09 14:19:25 +0530

    Time: 19 ms (44.44%) | Memory: 19.3 MB (96.30%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
@@ -0,0 +1,23 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def countCompleteSubarrays(self, A: List[int]) -> int:
+        n, k = len(A), len(set(A))
+        res = i = 0
+        count = Counter()
+
+        for j in range(n):
+            count[A[j]] += 1
+
+            while len(count) == k:
+                count[A[i]] -= 1
+
+                if count[A[i]] == 0:
+                    del count[A[i]]
+
+                i += 1
+
+            res += i
+
+        return res
